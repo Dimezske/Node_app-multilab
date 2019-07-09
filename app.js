14faa07104c46760e50b6172e21dd7c2c62a1b4f
BuildTools <unconfigured@null.spigotmc.org>
2019-07-09 23:27:31 +1000

    -All

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require("fs"); 
 var app = express();
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'));
+app.get('/index.html', function (req, res){
+   res.sendFile(__dirname + '/' + "index.html" );
+});
+
+app.get('/process_get', function (req,res){
+   // Prepare out put of fields in JSON Format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.body.first_name,
+      last_name:req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
 
 // This responds with "Hello World" on the homepage
 app.get('/', function (req, res) {
@@ -31,7 +61,60 @@ app.get('/ab*cd', function(req, res) {
    res.send('Page Pattern Match');
 })
 
-var server = app.listen(80, function () {
+// RestAPI -
+
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/databases/JSON/" + "users.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
+})
+
+
+var user = {
+   "user4" : {
+      "name" : "mohit",
+      "password" : "password4",
+      "profession" : "teacher",
+      "id": 4
+   }
+}
+
+app.post('/addUser', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/databases/JSON/" + "users.json", 'utf8', function (err, data) {
+      data = JSON.parse( data );
+      data["user4"] = user["user4"];
+      console.log( data );
+      res.end( JSON.stringify(data));
+   });
+})
+
+app.get('/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/databases/JSON/" + "users.json", 'utf8', function (err, data) {
+      var users = JSON.parse( data );
+      var user = users["user" + req.params.id] 
+      console.log( user );
+      res.end( JSON.stringify(user));
+   });
+})
+
+var id = 2;
+
+app.delete('/deleteUser', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      data = JSON.parse( data );
+      delete data["user" + 2];
+       
+      console.log( data );
+      res.end( JSON.stringify(data));
+   });
+})
+
+/* Server Connection*/
+var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
    
